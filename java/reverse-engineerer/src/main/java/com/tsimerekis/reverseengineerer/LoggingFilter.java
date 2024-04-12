78ff9d2d7e9c57b8fb1a1bb2ef1951664ce1def5
Nicholas Tsimerekis <ntsimerekis@gmail.com>
2024-04-11 21:15:58 -0700

    Fixed some errors

--- java/reverse-engineerer/src/main/java/com/tsimerekis/reverseengineerer/LoggingFilter.java
+++ java/reverse-engineerer/src/main/java/com/tsimerekis/reverseengineerer/LoggingFilter.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
